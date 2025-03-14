1c94fb8c5a0db86cdb0a501c76ac14d6b98322f5
Krovenx <Krovenx2@mail.ru>
2025-03-14 21:01:56 +0500

    Создал две переменные и вывел на экран сумму,разность,произвидение и частное с помощью alert()

--- skript.js
+++ skript.js
@@ -4,4 +4,15 @@ let b =Number(alert('20'));
 let age = ('Год выпуска первого Айфона');
 
 alert(age);
+
 let userName =(alert('Создатель языка JavaScript Виталий'));
+
+let num1 = 10;
+let num2 = 2;
+
+alert(
+    "  сумма: " + (num1 + num2) +
+    "  разность: " + (num1 - num2) +
+    "  умножение: " + (num1 * num2) +
+    "  деление: " + (num1 / num2)
+);
