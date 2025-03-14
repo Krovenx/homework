2fc39b00e3d488ff8f657f79591346e806480fc0
Krovenx <Krovenx2@mail.ru>
2025-03-14 21:32:52 +0500

    Вывел age с помощью prompt()

--- skript.js
+++ skript.js
@@ -1,9 +1,9 @@
 let a = Number(alert('10'));
 let b =Number(alert('20'));
 
-let age = ('Год выпуска первого Айфона');
+let ageIphone = ('Год выпуска первого Айфона');
 
-alert(age);
+alert(ageIphone);
 
 let userName =(alert('Создатель языка JavaScript Виталий'));
 
@@ -30,6 +30,6 @@ let num = 1;
  num -= 3; // Эквивалентно num = num - 3;
  num *= 7; // Эквивалентно num = num * 7;
  num /= 3; // Эквивалентно num = num / 3;
- num ++= ;  // Эквивалентно num = num + 1;
- num --= ;  // Эквивалентно num = num - 1;
 alert(num);
+
+let age = prompt('сколько вам лет?');
