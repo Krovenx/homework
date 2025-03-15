ccf15962507f406a2e3eee418d0d4da40c0bb8fc
Krovenx <Krovenx2@mail.ru>
2025-03-16 04:33:27 +0500

    Доработал JavaScript, доработал мобильную версию

--- skript.js
+++ skript.js
@@ -1,15 +1,13 @@
 let a = Number(alert('10'));
-let b =Number(alert('20'));
+let b = Number(alert('20'));
 
-let ageIphone = ('Год выпуска первого Айфона');
+let yearOfIssue = 2007;
+alert (`Год выпуска первого Айфона ${yearOfIssue}`);
 
-alert(ageIphone);
-
-let userName =(alert('Создатель языка JavaScript Виталий'));
+let userName = (alert('Создатель языка JavaScript Brendan Eich'));
 
 let num1 = 10;
 let num2 = 2;
-
 alert(
     "  сумма: " + (num1 + num2) +
     "  разность: " + (num1 - num2) +
@@ -18,28 +16,25 @@ alert(
 );
 
 let result = 2 ** 5;
-alert (result);
+alert(result);
 
 let c = Number('9');
 let d = Number('2');
-
-alert ("Остаток : " + (c % d));
+alert("Остаток : " + (c % d));
 
 let num = 1;
- num += 5; // Эквивалентно num = num + 5;
- num -= 3; // Эквивалентно num = num - 3;
- num *= 7; // Эквивалентно num = num * 7;
- num /= 3; // Эквивалентно num = num / 3;
+num += 5; 
+num -= 3; 
+num *= 7; 
+num /= 3; 
 alert(num);
 
-let age = prompt('сколько вам лет?');
-
+let age = prompt(`сколько вам лет?`);
 let user = {
- name: "Виталий" ,
- age:  "21",
- isAdmin:true
+    name: "Виталий",
+    age:`${age}` ,
+    isAdmin: true
 };
-
 console.log(user);
 alert(
     `Имя: ${user.name}\nВозраст: ${user.age}\nАдминистратор: ${user.isAdmin ? "Да" : "Нет"}`
