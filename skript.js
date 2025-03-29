6eeb7643fd81fb073667cdde8c4238f03bd79867
Krovenx <Krovenx2@mail.ru>
2025-03-29 13:46:59 +0500

    3 задание ДжаваСкрипт

--- skript.js
+++ skript.js
@@ -19,3 +19,13 @@ console.log(isEven (4));
 console.log(isEven(5));
 
 
+function printSquare(number) {
+    console.log(number * number);
+}
+printSquare(9);
+
+function printsSquare(number1) {
+    return number1 ** 2 ;
+}
+printSquare(8);
+
