aad43c9fa50f3a7e41a258e357ce41ae626dc210
Krovenx <Krovenx2@mail.ru>
2025-03-29 13:46:01 +0500

    2 задание ДжаваСкрипт

--- skript.js
+++ skript.js
@@ -8,4 +8,14 @@ function min(small, big) {
 console.log(min(8,4));
 console.log(min(6,6)); 
 
+function isEven (number) {
+    if (number % 2 == 0) {
+        return 'Число четное';
+    }else {
+        return 'Число нечетное';
+    }
+}
+console.log(isEven (4));
+console.log(isEven(5));
+
 
