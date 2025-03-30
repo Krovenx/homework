6b2bb5366660a3f67d180b85d8b04a37ce027d90
Krovenx <Krovenx2@mail.ru>
2025-03-30 12:55:03 +0500

    5 задание ДжаваСкрипт

--- skript.js
+++ skript.js
@@ -5,17 +5,17 @@ function min(small, big) {
         return small;
     }
 }
-console.log(min(8,4));
-console.log(min(6,6)); 
+console.log(min(8, 4));
+console.log(min(6, 6));
 
-function isEven (number) {
+function isEven(number) {
     if (number % 2 == 0) {
         return 'Число четное';
-    }else {
+    } else {
         return 'Число нечетное';
     }
 }
-console.log(isEven (4));
+console.log(isEven(4));
 console.log(isEven(5));
 
 
@@ -25,7 +25,7 @@ function printSquare(number) {
 printSquare(9);
 
 function printsSquare(number1) {
-    return number1 ** 2 ;
+    return number1 ** 2;
 }
 printSquare(8);
 
@@ -33,10 +33,21 @@ let age = prompt("Сколько вам лет");
 age = Number(age);
 if (age < 0) {
     alert('Вы ввели неправильное значение');
-}else if (age >= 0 && age <= 12) {
+} else if (age >= 0 && age <= 12) {
     alert('Привет, друг!');
 }
-else{
+else {
     alert('Добра пожаловать');
 }
 
+function multiplyNumbers(a ,b) {
+    const number1 = Number(a);
+    const number2 = Number(b);
+    if (isNaN(number1) || isNaN(number2)) {
+        return 'Одно или оба значения не являются числом';
+    }
+    return number1 * number2;
+}
+
+console.log(multiplyNumbers('qwer',5));
+console.log(multiplyNumbers('5',5));
