0ab70507af63a2706176010c7e6e6d4fba419097
Krovenx <Krovenx2@mail.ru>
2025-04-07 15:13:18 +0500

    4 задание

--- skript.js
+++ skript.js
@@ -8,5 +8,22 @@ let number = [1, 5, 4, 10, 0, 3];
 console.log(number[2]);
 
 let num = [1, 3, 5, 10, 20];
-num = num.join (' ');
+num = num.join(' ');
 console.log(num);
+
+let manyNum = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+];
+for (let item of manyNum ) {
+    for (let i = 0; i < item.length; i++) {
+        if (i === 0) {
+            console.log(`Массивы ${item[i]}`);
+        }else {
+            console.log (`\t ${item[i]}`);
+        }
+    }
+}
+
+
