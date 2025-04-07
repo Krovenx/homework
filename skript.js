71ec90b7c550e03902cc9a59e23384fda0339653
Krovenx <Krovenx2@mail.ru>
2025-04-07 15:01:31 +0500

    3 задание

--- skript.js
+++ skript.js
@@ -3,5 +3,10 @@ for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] == 0) break;
     console.log(numbers[i]);
 }
+
 let number = [1, 5, 4, 10, 0, 3];
 console.log(number[2]);
+
+let num = [1, 3, 5, 10, 20];
+num = num.join (' ');
+console.log(num);
