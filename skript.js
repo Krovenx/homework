107aa95827359355e25d0739635a6c8642b9f386
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:25:33 +0500

    4 задание

--- skript.js
+++ skript.js
@@ -1,7 +1,4 @@
-const number = 32.58884;
-const roundedDown = Math.floor(number);
-console.log("Округление до меньшего целого:", roundedDown);
-const roundedUp = Math.ceil(number);
-console.log("Округление до большего целого:", roundedUp);
-const rounded = Math.round(number);
-console.log("Округление до ближайшего целого:", rounded);
+const number = [52, 53, 49, 77, 21, 32];
+const minValue = Math.min(...number);
+const maxValue = Math.max(...number);
+console.log("минимальное значение: ", minValue, "максимальное значение", maxValue);    
