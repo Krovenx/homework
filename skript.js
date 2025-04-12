9ad47dd9071af8b12c6458490c86e1b1d311f38b
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:13:59 +0500

    2 задание

--- skript.js
+++ skript.js
@@ -1,3 +1,8 @@
-const str = 'js';
-const upperStr = str.toUpperCase();
-console.log(upperStr);
+function filterByPrefix(array, prefix) {
+    const lowerCasePrefix = prefix.toLowerCase();
+    return array.filter(item => item.toLowerCase().startsWith(lowerCasePrefix));
+}
+const strings = ["banana", "Apple", "apricot", "Avocado"];
+const prefix = "a";
+const result = filterByPrefix(strings, prefix);
+console.log(result);
