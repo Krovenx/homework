7002870e6f51cb4a9a549509dd85fe9ab7ae4cef
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:49:46 +0500

    8 задание

--- skript.js
+++ skript.js
@@ -1,8 +1,2 @@
-function getRandomInRange(min, max) {
-    if (min > max) {
-        [min, max] = [max, min];
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const randomNumber = getRandomInRange(5, 15);
-console.log("Случайное число в диапазоне от 5 до 15:", randomNumber);
+let currentDate = new Date();
+console.log("Текущая дата:", currentDate);
