aa68bf78d41b52967e71089a227411914d2e14fb
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:19:48 +0500

    3 задание

--- skript.js
+++ skript.js
@@ -1,8 +1,7 @@
-function filterByPrefix(array, prefix) {
-    const lowerCasePrefix = prefix.toLowerCase();
-    return array.filter(item => item.toLowerCase().startsWith(lowerCasePrefix));
-}
-const strings = ["banana", "Apple", "apricot", "Avocado"];
-const prefix = "a";
-const result = filterByPrefix(strings, prefix);
-console.log(result);
+const number = 32.58884;
+const roundedDown = Math.floor(number);
+console.log("Округление до меньшего целого:", roundedDown);
+const roundedUp = Math.ceil(number);
+console.log("Округление до большего целого:", roundedUp);
+const rounded = Math.round(number);
+console.log("Округление до ближайшего целого:", rounded);
