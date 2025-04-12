3b577b6b216893f0eec01be3f924f3b163559cd5
Krovenx <Krovenx2@mail.ru>
2025-04-12 16:08:44 +0500

    10 задание

--- skript.js
+++ skript.js
@@ -1,5 +1,23 @@
+function acceptsDateReturnFormat(date) {
+    const daysOfWeek = [
+        "воскресенье", "понедельник", "вторник",
+        "среда", "четверг", "пятница", "суббота"
+    ];
+    const months = [
+        "января", "февраля", "марта", "апреля", "мая", "июня",
+        "июля", "августа", "сентября", "октября", "ноября", "декабря"
+    ];
+
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+
+    console.log(`Дата: ${day} ${month} ${year} — это ${dayOfWeek}.
+Время: ${hours}:${minutes}:${seconds}`);
+}
 const currentDate = new Date();
-const come = new Date(currentDate);
-come.setDate(currentDate.getDate() + 73);
-console.log("Текущая дата: " + currentDate.toLocaleDateString("ru-RU"));
-console.log("Дата через 73 дня: " + come.toLocaleDateString("ru-RU"));
+acceptsDateReturnFormat(currentDate);
