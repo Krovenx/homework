3b3e4f3f159c0b992e4d7e5417cc2ae85e647d9a
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:39:30 +0500

    6 задание

--- skript.js
+++ skript.js
@@ -1,2 +1,11 @@
-const randomNumber = Math.floor(Math.random() * 10);
-console.log(randomNumber);
+function generateRandomArray(maxNumber) {
+    const arrayLength = Math.floor(maxNumber / 2);
+    const randomArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+      randomArray.push(Math.floor(Math.random() * (maxNumber + 1)));
+    }
+    return randomArray;
+  }
+const maxNumber = 10;
+const randomArray = generateRandomArray(maxNumber);
+console.log(randomArray);
