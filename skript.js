de5f5b6262b3328d513c54e7c3da69a6a9851b4e
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:54:48 +0500

    9 задание

--- skript.js
+++ skript.js
@@ -1,2 +1,5 @@
-let currentDate = new Date();
-console.log("Текущая дата:", currentDate);
+const currentDate = new Date();
+const come = new Date(currentDate);
+come.setDate(currentDate.getDate() + 73);
+console.log("Текущая дата: " + currentDate.toLocaleDateString("ru-RU"));
+console.log("Дата через 73 дня: " + come.toLocaleDateString("ru-RU"));
