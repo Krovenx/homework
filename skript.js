74d6bd4acda6731aac892f55193e5394d062f5bb
Krovenx <Krovenx2@mail.ru>
2025-04-12 14:28:00 +0500

    5 задание

--- skript.js
+++ skript.js
@@ -1,4 +1,2 @@
-const number = [52, 53, 49, 77, 21, 32];
-const minValue = Math.min(...number);
-const maxValue = Math.max(...number);
-console.log("минимальное значение: ", minValue, "максимальное значение", maxValue);    
+const randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
