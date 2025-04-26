4351f3e4f1ccfd46e8d49a339e7d69ce13f51dfa
Krovenx <Krovenx2@mail.ru>
2025-04-26 15:31:50 +0500

    4 задание

--- skript.js
+++ skript.js
@@ -1,8 +1,4 @@
-const interval = setInterval(() => {
-    console.log(new Date());
-}, 30000);
-setTimeout(() => {
-   clearInterval(intervalId); 
-   console.log('Время прошло');
-
-}, 30000);
+function delayForSecond(callback) {
+    setTimeout(callback,1000);
+    callback();
+}
