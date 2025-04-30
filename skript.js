1b0963f171fb00918a5aa8590186a968db8c92fc
Krovenx <krovenx3@mail.ru>
2025-04-30 20:54:37 +0500

    1-5 задание callback

--- skript.js
+++ skript.js
@@ -1,3 +1,47 @@
+// 1 задание
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ console.log(people.sort((a, b) => a.age - b.age));
+
+ // 2 Задание
+function isPositive(number) {
+    return number > 0;
+}
+function isMale(person) {
+    return person.gender === 'male';
+}
+function filter(array, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+ // 3 Задание
+const interval = setInterval(() => {
+    console.log(new Date());
+}, 30000);
+setTimeout(() => {
+   clearInterval(intervalId); 
+   console.log('Время прошло');
+
+}, 30000);
+
+ // 4 Задание
+function delayForSecond(callback) {
+    setTimeout(callback,1000);
+    callback();
+}
+console.log(filter([3, -4, 1, 9], isPositive));
+
+// 5 задание
 function delayForSecond(cb) {
     setTimeout(() => {
         console.log("прошла одна секунда");
